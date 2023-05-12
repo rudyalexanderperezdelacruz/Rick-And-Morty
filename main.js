b67d18b6df5d95ad82862f620bbc8c8918e3c5a7
ruddyalexander <ruddyalexander8@gmail.com>
2023-05-12 13:14:40 -0400

    upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function getCharacters(done) {
+
+    const results = fetch("https://rickandmortyapi.com/api/character")
+
+    results
+    .then(response => response.json())
+    .then(data => {
+      done(data)
+    });
+}
+
+getCharacters(data => {
+
+    data.results.forEach(personaje => {
+  
+      const article = document.createRange().createContextualFragment(/*html*/`
+        <article>
+  
+        <div class="image-container">
+          <img src="${personaje.image}" alt="">
+        </div>
+  
+        <h2>${personaje.name}</h2>
+        <span>Status: ${personaje.status}</span>
+  
+      </article>
+        `);
+  
+      const main = document.querySelector("main");
+  
+      main.append(article);
+  
+    })
+  
+  });
